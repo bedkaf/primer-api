e5f3cf929bc6c5f03cb16698bb50d85e246dfc05
Hamilton B. Mosquera <hammosquera@gmail.com>
2022-12-13 11:56:07 -0500

    end point complete
    I developed my first API where I have 3 end points that are responsible for giving different types of privileges, the code can be improved, changes will come later as I master more this technology.

---
+++ routes/auth.js
@@ -0,0 +1,45 @@
+import { Router } from "express";
+import auttByEmailPwd from "../helpers/auth-by-email-pwd.js";
+
+const authRRouter = Router();
+
+// Endpoint publico (no autorizado y no atenticado)
+authRRouter.get("/publico", (req, res) => {
+  res.send("Esta es la vista del piblico");
+})
+
+// Endpoint autenticado
+authRRouter.post("/autenticado", (req, res) => {
+  const {email, password} = req.body;
+
+  if(!email || !password) return res.sendStatus(400);
+  try{
+    const user = auttByEmailPwd(email, password);
+    return res.send(`Hola ${user.name} estas autentificado`);
+
+  }catch(err){
+    return res.sendStatus(401);
+  }
+
+})
+
+// Endpoint autorizado
+authRRouter.post("/autorizado", (req, res) => {
+  const {email, password} = req.body;
+
+  if(!email || !password) return res.sendStatus(400);
+
+  try{
+    const user = auttByEmailPwd(email, password);
+   
+    if(user.role !== "admin") return res.send(403);
+
+    return res.send(`Hola ${user.name} estas autentificado y autorizado`);
+
+  }catch(err){
+    return res.sendStatus(401);
+  }
+
+})
+
+export default authRRouter;

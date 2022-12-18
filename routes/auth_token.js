271b65d08f9a607218726d2cd9c8aef84bf91837
Hamilton B. Mosquera <hammosquera@gmail.com>
2022-12-18 00:34:50 -0500

    complete

--- routes/auth_token.js
+++ routes/auth_token.js
@@ -1,19 +1,56 @@
 import { Router } from "express";
+import { USER_BD } from "../bbdd.js";
 import auttByEmailPwd from "../helpers/auth-by-email-pwd.js";
+import { SignJWT, jwtVerify } from "jose";
 
 const authTokenRouter = Router();
 
-authTokenRouter.post("/login", (req, res) => {
-  const {email, passord} = req.body;
+//Login con Emay y Password
+authTokenRouter.post("/login", async (req, res) => {
+  const {email, password} = req.body;
 
-  if(!email || !passord) return res.sendStatus(400);
+  if(!email || !password) return res.sendStatus(400);
 
   try{
-    const user =  auttByEmailPwd(email, passord);
-    return res.send(`Usuario ${user.name} autentificado`);
+    const {guid} =  auttByEmailPwd(email, password);
+    
+    //Generar token y devolver token
+    const encoder = new TextEncoder();
+    const jwtConstructor = new SignJWT({guid});
+    const jwt = await jwtConstructor
+      .setProtectedHeader({alg:"HS256", typ:"JWT"})
+      .setIssuedAt()
+      .setExpirationTime('1h').sign(encoder.encode(process.env.JWT_PRIVATE_KEY));
+
+    return res.send({jwt});
   }catch(err){
     return res.sendStatus(401);
   }
+});
+
+//Solicitud autentificada con el token, se obtiene el perfil del usuario
+authTokenRouter.get("/profile", async (req, res) => {
+  const { authorization } = req.headers;
+
+  if(!authorization) return res.sendStatus(401);
+
+  try {
+    const encoder = new TextEncoder();
+
+    const {payload} = await jwtVerify(authorization, encoder.encode(process.env.JWT_PRIVATE_KEY));
+
+    const user = USER_BD.find((element) => element.guid === payload.guid);
+    if (!user) return res.sendStatus(401);
+
+    delete user.password;
+    
+    res.send(user);
+    } catch (err) {
+    return res.sendStatus(401);
+  }
+
+  //Obtener token de cabecera y comprobar su autenticidad
+
 });
 
 export default authTokenRouter;
